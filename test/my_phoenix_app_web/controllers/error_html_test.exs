dca2619fae74c3009dcfdda9bb7b5946e847795b
Richard Kubina <rjkubina@gmail.com>
2025-09-25 13:29:37 -0400

    mix format runs on save now..

--- test/my_phoenix_app_web/controllers/error_html_test.exs
+++ test/my_phoenix_app_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule MyPhoenixAppWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(MyPhoenixAppWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(MyPhoenixAppWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
